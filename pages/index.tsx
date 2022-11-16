1d5f45a95e91e9a0980f398c479ac69db0af88f9
Alejandro <alejandromanuelhernandez@gmail.com>
2022-11-16 14:44:43 -0300

    add marquee

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,18 +10,25 @@ const Home: NextPage = () => {
     <div className="h-full bg-black">
       <header className="header sticky top-0 flex items-center justify-between">
         <div className="fixed top-0 mt-10 ml-64 pl-2">
-        <Image alt="Basement" src={logo} />
-        <button className="border-2 border-white rounded-full text-xl pt-2 pb-2 pl-8 pr-8 fixed right-0 ml-8 mr-64">CART (0)</button>
+          <Image alt="Basement" src={logo} />
+          <button className="border-2 border-white rounded-full text-xl pt-2 pb-2 pl-8 pr-8 fixed right-0 ml-8 mr-64">
+            CART(0)
+          </button>
         </div>
         <div className="mx-auto mt-32">
-        <Image alt="Basement Supply" src={header} />
+          <Image alt="Basement Supply" src={header} />
+          <hr className="border-1 border-white border-solid w-full mt-10" />
+          <h1 className="text-4xl mt-3 max-w-34  animate-marquee whitespace-nowrap">
+            A man can´t have enough basement. swag - A man can´t have enough basement. swag
+          </h1>
+          <hr className="border-1 border-white border-solid w-full mt-3" />
         </div>
       </header>
-      <footer className="footer sticky bottom-0 flex items-center justify-between">
+      {/* <footer className="footer sticky bottom-0 flex items-center justify-between">
         <div className="mx-auto">
-      <Image alt="Wear Everyday" src={footer} />
-      </div>
-      </footer>
+          <Image alt="Wear Everyday" src={footer} />
+        </div>
+      </footer> */}
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        marquee: "marquee 25s linear infinite",
+        marquee2: "marquee2 25s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": {transform: "translateX(0%)"},
+          "100%": {transform: "translateX(-100%)"},
+        },
+      },
+    },
   },
-  variants: {
-    extend: {},
-  },
+  variants: {},
   plugins: [],
-}
+};
